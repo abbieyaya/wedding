bfbec8353f59da2bbf4dc272c9c9f215e35bfee5
Abbie Chang <abbie.chang@Cortina-Access.com>
2022-08-18 13:04:31 +0800

    test map

--- index.js
+++ index.js
@@ -89,6 +89,106 @@ function handleEvent(event) {
 					  }
 			]);
   }
+  
+  if (event.message.text === '測試1') {
+        return client.replyMessage(event.replyToken, [
+            {
+                type: 'sticker',
+                packageId: '1',
+                stickerId: '1'
+            },
+            {
+                type: 'image',
+                originalContentUrl: 'https://developers.line.biz/media/messaging-api/messages/image-full-04fbba55.png',
+                previewImageUrl: 'https://developers.line.biz/media/messaging-api/messages/image-167efb33.png'
+            },
+            {
+                type: 'video',
+                originalContentUrl: 'https://www.sample-videos.com/video123/mp4/240/big_buck_bunny_240p_1mb.mp4',
+                previewImageUrl: 'https://www.sample-videos.com/img/Sample-jpg-image-50kb.jpg'
+            },
+            {
+                type: 'audio',
+                originalContentUrl: 'https://www.sample-videos.com/audio/mp3/crowd-cheering.mp3',
+                duration: '27000'
+            },
+            {
+                type: 'location',
+                title: 'my location',
+                address: "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+                latitude: 35.65910807942215,
+                longitude: 139.70372892916203
+            }
+        ]);
+    }
+	
+    if (event.message.text === '測試2') {
+        return client.replyMessage(event.replyToken, [
+            {
+                type: 'imagemap',
+                baseUrl: 'https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/rich',
+                altText: 'Imagemap alt text',
+                baseSize: { width: 1040, height: 1040 },
+                actions: [
+                    { area: { x: 0, y: 0, width: 520, height: 520 }, type: 'uri', linkUri: 'https://store.line.me/family/manga/en' },
+                    { area: { x: 520, y: 0, width: 520, height: 520 }, type: 'uri', linkUri: 'https://store.line.me/family/music/en' },
+                    { area: { x: 0, y: 520, width: 520, height: 520 }, type: 'uri', linkUri: 'https://store.line.me/family/play/en' },
+                    { area: { x: 520, y: 520, width: 520, height: 520 }, type: 'message', text: 'URANAI!' },
+                ],
+                video: {
+                    originalContentUrl: 'https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/imagemap/video.mp4',
+                    previewImageUrl: 'https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/imagemap/preview.jpg',
+                    area: {
+                        x: 280,
+                        y: 385,
+                        width: 480,
+                        height: 270,
+                    },
+                    externalLink: {
+                        linkUri: 'https://line.me',
+                        label: 'LINE'
+                    }
+                },
+            },
+            {
+                type: 'template',
+                altText: 'Buttons alt text',
+                template: {
+                    type: 'buttons',
+                    thumbnailImageUrl: 'https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/buttons/1040.jpg',
+                    title: 'My button sample',
+                    text: 'Hello, my button',
+                    actions: [
+                        { label: 'Go to line.me', type: 'uri', uri: 'https://line.me' },
+                        { label: 'Say hello1', type: 'postback', data: 'hello こんにちは' },
+                        { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
+                        { label: 'Say message', type: 'message', text: 'Rice=米' },
+                    ],
+                },
+            },
+            {
+                type: 'flex',
+                altText: 'this is a flex message',
+                contents: {
+                    type: 'bubble',
+                    body: {
+                        type: 'box',
+                        layout: 'vertical',
+                        contents: [
+                            {
+                                type: 'text',
+                                text: 'hello'
+                            },
+                            {
+                                type: 'text',
+                                text: 'world'
+                            }
+                        ]
+                    }
+                }
+            }
+        ]);
+    }
 }
 
 // listen on port
