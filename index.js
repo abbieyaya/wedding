f4fac6cbda601550126d7848ba2d7b8a8d19f8a3
Abbie Chang <abbie.chang@Cortina-Access.com>
2022-08-18 12:55:17 +0800

    test photo

--- index.js
+++ index.js
@@ -52,6 +52,43 @@ function handleEvent(event) {
 					  }
 			]);
   }
+  
+  if (event.message.text == '婚紗') {
+		  return client.replyMessage(event.replyToken, [
+					  {
+						type: 'template',
+						template: {
+							type: "image_carousel",
+							columns: [
+							  {
+								imageUrl: "https://example.com/bot/images/item1.jpg",
+								action: {
+								  type: "postback",
+								  label: "Buy",
+								  data: "action=buy&itemid=111"
+								}
+							  },
+							  {
+								imageUrl: "https://example.com/bot/images/item2.jpg",
+								action: {
+								  type: "message",
+								  label: "Yes",
+								  text: "yes"
+								}
+							  },
+							  {
+								imageUrl: "https://example.com/bot/images/item3.jpg",
+								action: {
+								  type: "uri",
+								  label: "View detail",
+								  uri: "http://example.com/page/222"
+								}
+							  }
+							]
+						  }
+					  }
+			]);
+  }
 }
 
 // listen on port
