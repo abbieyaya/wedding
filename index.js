2e33f120dd83d5718fbdd494e9ca757aad8aa286
Abbie Chang <abbie.chang@Cortina-Access.com>
2022-08-24 12:38:36 +0800

    test map

--- index.js
+++ index.js
@@ -298,3 +298,115 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
+
+
+const { join } = require("path");
+const { readFileSync } = require("fs");
+
+const richMenuObjectA = () => ({
+  size: {
+    width: 2500,
+    height: 1686
+  },
+  selected: false,
+  name: "richmenu-a",
+  chatBarText: "Tap to open",
+  areas: [
+    {
+      bounds: {
+        x: 0,
+        y: 0,
+        width: 1250,
+        height: 1686
+      },
+      action: {
+        type: "uri",
+        uri: "https://www.line-community.me/"
+      }
+    },
+    {
+      bounds: {
+        x: 1251,
+        y: 0,
+        width: 1250,
+        height: 1686
+      },
+      action: {
+        type: "richmenuswitch",
+        richMenuAliasId: "richmenu-alias-b",
+        data: "richmenu-changed-to-b"
+      }
+    }
+  ]
+})
+
+const richMenuObjectB = () => ({
+  size: {
+    width: 2500,
+    height: 1686
+  },
+  selected: false,
+  name: "richmenu-b",
+  chatBarText: "Tap to open",
+  areas: [
+    {
+      bounds: {
+        x: 0,
+        y: 0,
+        width: 1250,
+        height: 1686
+      },
+      action: {
+        type: "richmenuswitch",
+        richMenuAliasId: "richmenu-alias-a",
+        data: "richmenu-changed-to-a"
+      }
+    },
+      {
+      bounds: {
+        x: 1251,
+        y: 0,
+        width: 1250,
+        height: 1686
+      },
+      action: {
+        type: "uri",
+        uri: "https://www.line-community.me/"
+      }
+    }
+  ]
+})
+
+const main = async (client) => {
+  // 2. Create rich menu A (richmenu-a)
+  const richMenuAId = await client.createRichMenu(
+    richMenuObjectA()
+  )
+
+  // 3. Upload image to rich menu A
+  const filepathA = join(__dirname, './public/richmenu-a.png')
+  const bufferA = readFileSync(filepathA)
+
+  await client.setRichMenuImage(richMenuAId, bufferA)
+
+  // 4. Create rich menu B (richmenu-b)
+  const richMenuBId = await client.createRichMenu(richMenuObjectB())
+
+  // 5. Upload image to rich menu B
+  const filepathB = join(__dirname, './public/richmenu-b.png')
+  const bufferB = readFileSync(filepathB);
+
+  await client.setRichMenuImage(richMenuBId, bufferB);
+
+  // 6. Set rich menu A as the default rich menu
+  await client.setDefaultRichMenu(richMenuAId)
+
+  // 7. Create rich menu alias A
+  await client.createRichMenuAlias(richMenuAId, 'richmenu-alias-a')
+
+  // 8. Create rich menu alias B
+  await client.createRichMenuAlias(richMenuBId, 'richmenu-alias-b')
+  console.log('success')
+}
+
+main(client)
