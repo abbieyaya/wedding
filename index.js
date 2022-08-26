3a5f44b9d113f07bb3329d713f5714bc0affe8d5
Abbie Chang <abbie.chang@Cortina-Access.com>
2022-08-26 11:41:49 +0800

    test map

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const config = {
 // create LINE SDK client
 const client = new line.Client(config);
 
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
@@ -295,11 +298,8 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
-
-// create Express app
-// about Express itself: https://expressjs.com/
-const app = express();
 /*
+
 const { join } = require("path");
 const { readFileSync } = require("fs");
 
