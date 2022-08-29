d8b2a0e03b48fadd3278b935bc000f347ec51429
Abbie Chang <abbie.chang@Cortina-Access.com>
2022-08-29 12:05:18 +0800

    test map

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function handleEvent(event) {
 					  },
 					  {
 						imageUrl: 'https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/imagemap/preview.jpg',
-						action: { label: '查看更多', type: 'uri', uri: 'https://line.me' }
+						action: { label: '查看更多', type: 'uri', uri: './photo.html' }
 					  }
 					]
 				},
