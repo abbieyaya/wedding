f513a949870f38dec1908cdabd33152986cf7d64
Abbie Chang <abbie.chang@Cortina-Access.com>
2022-08-30 14:54:23 +0800

    more sample

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function handleEvent(event) {
 						action: { label: '查看更多', type: 'uri', uri: 'https://wedding-photo.onrender.com/test.html' }
 					  },
 					  {
-						imageUrl: 'https://github.com/abbieyaya/wedding_photo/raw/main/Role1.jpg',
+						imageUrl: 'https://raw.githubusercontent.com/abbieyaya/wedding_photo/main/Role1.jpg?token=GHSAT0AAAAAABYE3D6D5CMPNJFSS7MUIQYAYYNWORA',
 						action: { label: '查看更多', type: 'uri', uri: 'https://wedding-photo.onrender.com/ori.html' }
 					  }
 					]
